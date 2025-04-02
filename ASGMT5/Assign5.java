5fe3a382351f477999618552b1f0a47a6c212e24
Ry-256 <181497212+Ry-256@users.noreply.github.com>
2025-04-02 16:50:36 -0400

    Added loop for files and some logic
    i think im wrong about using a linked list to track the values for max summation...
    need help...
    I was thinking having a 2d array to track max sum at each index and saving index that has largest summation then print that column...

--- ASGMT5/Assign5.java
+++ ASGMT5/Assign5.java
@@ -1,22 +1,74 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Assign5 {
 
     public static void main(String[] args) throws FileNotFoundException {
         int[] x;
-        x = fileInts();
-        print(x, 0, x.length-1);
 
+        for (int k = 0; k < 4; k++) {
+
+            x = fileInts();
+
+            print(x, 0, x.length - 1);
+
+            int max_Sub = findMaxSubSeq(x);
+
+            System.out.print("Max Sum Sequence : " + max_Sub + " : ");
+
+            printLinkedList(maxSum);
+
+            System.out.println();
+        }
 
     }
 
-    public static void findMaxSubSeq(int[] y) {
-        
+    public static LinkedList<Integer> maxSum = new LinkedList<>();
+
+    public static int findMaxSubSeq(int[] y) {
+        int max = 0;
+        int[] tableSum = new int[y.length];
+
+        for (int i = 0; i < y.length; i++) {
+
+            tableSum[i] = y[i];
+
+            for (int j = 0; j < i; j++) {
+
+                if (y[j] <= y[i]) {
+
+                    tableSum[i] = Math.max(tableSum[i], y[i] + tableSum[j]);
+
+                }
+
+            }
+            max = Math.max(max, tableSum[i]);
+
+        }
+
+        return max;
 
     }
 
+    public static void printLinkedList(LinkedList<Integer> x) {
+
+        System.out.print("[ ");
+
+        for (int i = 0; i < x.size(); i++) {
+
+            System.out.print(x.get(i));
+
+            if (i < x.size() - 1) {
+                System.out.print(", ");
+            } else {
+                System.out.print(" ");
+            }
+
+        }
+        System.err.println("]");
+    }
 
     // Done : builds array from file correctly//
     public static int[] fileInts() throws FileNotFoundException {
@@ -30,7 +82,6 @@ public class Assign5 {
                 System.out.print("Input File Name : ");
                 userVar = scnr.next();
             }
-            System.out.println();
             Scanner scnrX = new Scanner(new File(userVar));
 
             // Grab size n//
@@ -39,13 +90,13 @@ public class Assign5 {
             int x[] = new int[n];
 
             for (n = 0; n < x.length; n++) {
-                
-                    if (scnrX.hasNextInt()) {
-                        x[n] = scnrX.nextInt();
-                    } else {// Out Of Room//
-                        n = x.length;
-                    }
-                
+
+                if (scnrX.hasNextInt()) {
+                    x[n] = scnrX.nextInt();
+                } else {// Out Of Room//
+                    n = x.length;
+                }
+
             }
 
             return x;
@@ -55,11 +106,11 @@ public class Assign5 {
         }
     }
 
-    //Done : Array printing, Use index values in call//
+    // Done : Array printing, Use index values in call//
     public static void print(int[] y, int start, int end) {
-        System.out.print("Array Size: " + (end - start + 1) + "  : ");
-
-        System.out.print("( " + start + ", " + end + " ) : " + "[ ");
+        System.out.print("Array Size: " + (end - start + 1) + " : ");
+        // System.out.print("( " + start + ", " + end + " ) : " + "[ ");
+        System.out.print(" [ ");
 
         for (int i = start; i <= (end); i++) {
 
@@ -74,3 +125,19 @@ public class Assign5 {
         System.err.println("]");
     }
 }
+/* 'Assign5' 
+
+Input File Name : input-5.1.txt
+Array Size: 6 :  [ 1, 14, 5, 5, 2, 5 ]
+Max Sum Sequence : 16 : [ ]
+
+Input File Name : input-5.2.txt
+Array Size: 12 :  [ 41, 18467, 6334, 26500, 19169, 15724, 11478, 29358, 26962, 24464, 30000, 30000 ]
+Max Sum Sequence : 134366 : [ ]
+
+Input File Name : input-5.3.txt
+Array Size: 12 :  [ 50000, 25053, 4601, 4540, 20255, 23073, 17419, 10282, 3621, 32092, 945, 50000 ]
+Max Sum Sequence : 130021 : [ ]
+
+PS C:\Users\Rwils\Desktop\Assignment5> 
+ */
