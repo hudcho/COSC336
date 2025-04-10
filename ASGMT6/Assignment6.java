b93b7c70b085c8e9be7a7807b030d46857453823
Ry-256 <181497212+Ry-256@users.noreply.github.com>
2025-04-09 22:21:51 -0400

    Done
    ill pick something else...

--- ASGMT6/Assignment6.java
+++ ASGMT6/Assignment6.java
@@ -1,25 +1,64 @@
 
 public class Assignment6{
     public static void main(String[] args){
-        BST<Integer> test_BST = new BST<>();
+        BST<Integer> treeEX = new BST<>();
 
-        test_BST.insert(50);
-        test_BST.insert(30);
-        test_BST.insert(20);
-        test_BST.insert(20);
-        test_BST.insert(40);
-        test_BST.insert(70);
-        test_BST.insert(60);
-        test_BST.insert(80);
+        treeEX.insert(50);
+        treeEX.insert(30);
+        treeEX.insert(20);
+        treeEX.insert(20);
+        treeEX.insert(40);
+        treeEX.insert(70);
+        treeEX.insert(60);
+        treeEX.insert(80);
 
-        //test_BST.inorder();
-        //System.out.println();
-        test_BST.preorder();
-        System.out.println();
+        //treeEX.inorder();
+        System.out.println("Pre order");
+        treeEX.preorder();
+
+        System.out.println("\nLeft rotate root");
+        treeEX.rotateL_Root();
+        treeEX.preorder();
+
+        System.out.println("\nRight rotate root");
+        treeEX.rotateR_Root();
+        treeEX.preorder();
 
 
         
     }
+    /* Assignment6' 
+        Pre order
+        Value :50       Size Node : 8
+        Value :30       Size Node : 4
+        Value :20       Size Node : 2
+        Value :20       Size Node : 1
+        Value :40       Size Node : 1
+        Value :70       Size Node : 3
+        Value :60       Size Node : 1
+        Value :80       Size Node : 1
+
+        Left rotate root
+        Value :70       Size Node : 8
+        Value :50       Size Node : 6
+        Value :30       Size Node : 4
+        Value :20       Size Node : 2
+        Value :20       Size Node : 1
+        Value :40       Size Node : 1
+        Value :60       Size Node : 1
+        Value :80       Size Node : 1
+
+        Right rotate root
+        Value :50       Size Node : 8
+        Value :30       Size Node : 4
+        Value :20       Size Node : 2
+        Value :20       Size Node : 1
+        Value :40       Size Node : 1
+        Value :70       Size Node : 3
+        Value :60       Size Node : 1
+        Value :80       Size Node : 1
+        \ASGMT6> 
+     */
     
 }
 
--- ASGMT6/BST.java
+++ ASGMT6/BST.java
@@ -1,5 +1,3 @@
-
-
 public class BST<T extends Comparable<T>> {
 
     private class Node<T> {
@@ -9,8 +7,7 @@ public class BST<T extends Comparable<T>> {
         protected Node<T> left;
         protected Node<T> right;
 
-        public Node(T new_item)
-        {
+        public Node(T new_item) {
             this.size = 1;
             this.item = new_item;
             this.left = null;
@@ -21,72 +18,127 @@ public class BST<T extends Comparable<T>> {
 
     private Node<T> head;
 
-    public BST(){
+    public BST() {
         this.head = null;
     }
 
-    public void insert(T item){
-        
-        if (head == null){
+    public void insert(T item) {
+
+        if (head == null) {
             head = new Node<>(item);
-        }
-        else{
+        } else {
             Node<T> curr = this.head;
             insert_Private(curr, item);
         }
 
     }
-    private Node<T> insert_Private(Node<T> run, T k){
-        if(run == null){
+
+    private Node<T> insert_Private(Node<T> run, T k) {
+        if (run == null) {
             return new Node<>(k);
         }
-        //Increment size of the (non.root && not null node) by +1//
+
+        // Increment size of the (non.root && not null node) by +1//
         run.size += 1;
 
         Comparable<T> runItem = (Comparable<T>) run.item;
-        if(runItem.compareTo(k) >= 0){
+        if (runItem.compareTo(k) >= 0) {
             run.left = insert_Private(run.left, k);
-        }
-        else{
+        } else {
             run.right = insert_Private(run.right, k);
         }
-        
 
         return run;
     }
 
-    public void inorder(){
-        if (head == null){
+    public void rotateL_Root() {
+        if (head == null) {
             System.out.println("ERROR DEAD TREE");
+        } else {
+            Node<T> curr = this.head;
+            head = rotateL_Private(curr);
         }
-        else{
+
+    }
+
+    private Node<T> rotateL_Private(Node<T> curr) {
+        if (curr.right != null) {
+
+            Node<T> topRight = curr.right;
+            curr.right = topRight.left;
+            topRight.left = curr;
+
+            curr.size = 1 + getSize(curr.left) + getSize(curr.right);
+            topRight.size = 1 + getSize(topRight.left) + getSize(topRight.right);
+
+            return topRight;
+        }
+        return curr;
+    }
+
+
+    public void rotateR_Root() {
+        if (head == null) {
+            System.out.println("ERROR DEAD TREE");
+        } else {
+            Node<T> curr = this.head;
+            head = rotateR_Private(curr);
+        }
+
+    }
+
+    private Node<T> rotateR_Private(Node<T> curr) {
+        if (curr.left != null) {
+
+            Node<T> topRight = curr.left;
+            curr.left = topRight.right;
+            topRight.right = curr;
+
+            curr.size = 1 + getSize(curr.left) + getSize(curr.right);
+            topRight.size = 1 + getSize(topRight.left) + getSize(topRight.right);
+
+            return topRight;
+        }
+        return curr;
+    }
+
+    private int getSize(Node<T> node) {
+        return node == null ? 0 : node.size;
+    }
+
+    public void inorder() {
+        if (head == null) {
+            System.out.println("ERROR DEAD TREE");
+        } else {
             Node<T> curr = this.head;
             inorder_Private(curr);
         }
 
     }
-    private void inorder_Private(Node<T> curr){
+
+    private void inorder_Private(Node<T> curr) {
 
         if (curr != null) {
             inorder_Private(curr.left);
-            System.out.println("Size Node : "+curr.size+"\tValue :"+(curr.item.toString()) + " ");
+            System.out.println("Size Node : " + curr.size + "\tValue :" + (curr.item.toString()) + " ");
             inorder_Private(curr.right);
         }
     }
-    public void preorder(){
-        if (head == null){
+
+    public void preorder() {
+        if (head == null) {
             System.out.println("ERROR DEAD TREE");
-        }
-        else{
+        } else {
             Node<T> curr = this.head;
             preorder_Private(curr);
         }
 
     }
-    private void preorder_Private(Node<T> curr){
+
+    private void preorder_Private(Node<T> curr) {
 
         if (curr != null) {
-            System.out.println("Value :"+(curr.item.toString()) +"\tSize Node : "+curr.size);
+            System.out.println("Value :" + (curr.item.toString()) + "\tSize Node : " + curr.size);
             preorder_Private(curr.left);
             preorder_Private(curr.right);
         }
