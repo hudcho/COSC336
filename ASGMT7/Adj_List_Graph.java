dcdc9323611b18a9024e3e7669dc93e79c7da913
Ry-256 <181497212+Ry-256@users.noreply.github.com>
2025-04-18 16:08:54 -0400

    Done?

--- ASGMT7/Adj_List_Graph.java
+++ ASGMT7/Adj_List_Graph.java
@@ -1,75 +1,132 @@
-import java.util.*;
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Scanner;
 
-public class Adj_List_Graph{
-    int n;  // no of nodes
-    ArrayList<ArrayList<Integer> > adj; 
-    
-    //constructor taking as the single parameter the number of nodes
-    Adj_List_Graph(int no_nodes) {
+public class Adj_List_Graph {
+
+
+   int n;
+   ArrayList<ArrayList<Integer>> adj;
+
+
+   Adj_List_Graph(int no_nodes) {
       n = no_nodes;
-      adj =  new ArrayList<ArrayList<Integer> >(n);
-      for (int i = 0; i < n; i++)
-        adj.add(new ArrayList<Integer>());
-      
-    }
-    
-    
-    // A utility function to add an edge in an
-    // undirected graph; for directed graph remove the second line
-    public void addEdge(int u, int v)
-    {
-      adj.get(u).add(v);
-      // adj.get(v).add(u);  //this line should be un-commented, if graph is undirected
-    }
-    
-    // A utility function to print the adjacency list
-    // representation of graph
-    
-    public void printGraph()
-    {
-      for (int i = 0; i < n; i++) {
-        System.out.println("\nAdjacency list of vertex" + i);
-        System.out.print("head");
-        for (int j = 0; j < adj.get(i).size(); j++) {
-          System.out.print(" -> "+adj.get(i).get(j));
-        }
-        System.out.println();
-      }
-    }
 
-    public static Adj_List_Graph file_Intake() throws FileNotFoundException {
+      adj = new ArrayList<>(n);
+
+      for (int i = 0; i < n; i++) {
+         adj.add(new ArrayList<>());
+      }
+
+   }
+
+
+   public Adj_List_Graph graphSquared() {
+
+      Adj_List_Graph squareGraph = new Adj_List_Graph(n);
+      LinkedList<Integer> pathFrom = new LinkedList<>();
+      int[] pathLength = new int[n];
+
+      for (int currBFS = 0; currBFS < n; ++currBFS) {
+
+         pathFrom.clear();
+         pathFrom.add(currBFS);
+
+         for (int k = 0; k < n; k++) {
+            pathLength[k] = -1;
+         }
+         pathLength[currBFS] = 0;
+
+         while (!pathFrom.isEmpty()) {
+
+            int fromNode = pathFrom.removeFirst();
+
+            if (pathLength[fromNode] < 2) {
+               for (int j = 0; j < adj.get(fromNode).size(); j++) {
+
+                  int toNode = adj.get(fromNode).get(j);
+
+                  if (pathLength[toNode] == -1) {
+
+                     pathLength[toNode] = pathLength[fromNode] + 1;
+
+                     pathFrom.addLast(toNode);
+
+                     if (pathLength[toNode] <= 2) {
+                        squareGraph.addEdge(currBFS, adj.get(fromNode).get(j));
+                     }
+
+                  }
+
+               }
+            }
+
+         }
+
+      }
+
+      return squareGraph;
+   }
+
+
+   public void addEdge(int u, int v) {
+      adj.get(u).add(v);
+
+      // this line should be un-commented, if graph is undirected
+      // adj.get(v).add(u);
+   }
+
+
+   public void printGraph() {
+      for (int i = 0; i < n; i++) {
+
+         System.out.print("\nAdjacency list of vertex " + i + "  :  ");
+         System.out.print("head");
+
+         for (int j = 0; j < adj.get(i).size(); j++) {
+            System.out.print(" -> " + adj.get(i).get(j));
+         }
+         // System.out.println();
+      }
+   }
+
+
+   public static Adj_List_Graph file_Intake() throws FileNotFoundException {
       Scanner scnr = new Scanner(System.in);
-  
+
       System.out.print("Input File Name : ");
       String userVar = scnr.next();
-  
-      try {
-        while (!new File(userVar).exists()) {
-          System.out.print("Input File Name : ");
-          userVar = scnr.next();
-        }
-        System.out.println();
-        Scanner scnrX = new Scanner(new File(userVar));
-  
-        int n = scnrX.nextInt();
-        Adj_List_Graph x = new Adj_List_Graph(n);
-  
-        int k = 0;
-        
-        while (scnrX.hasNextInt()) {
-          int key = k / n;
-          int in = scnrX.nextInt();
-          if (in == 1) {
-            x.addEdge(key, k % n);
-          }
-          k++;
-        }
-        return x;
-      } catch (FileNotFoundException e) {
-        System.out.println("Error File Not Found...");
-        return null;
-      }
-    }
 
-  }
+      try {
+         while (!new File(userVar).exists()) {
+            scnr.nextLine();
+            System.out.print("Input File Name : ");
+            userVar = scnr.next();
+         }
+         // System.out.println();
+         Scanner scnrX = new Scanner(new File(userVar));
+
+         int n = scnrX.nextInt();
+         Adj_List_Graph x = new Adj_List_Graph(n);
+
+         int k = 0;
+
+         while (scnrX.hasNextInt()) {
+            int key = k / n;
+            int in = scnrX.nextInt();
+            // System.out.println("KEY = "+key+" : IN : " +in+" : k = "+k+"");
+            if (in == 1) {
+               x.addEdge(key, k % n);
+            }
+            k++;
+         }
+         return x;
+      } catch (FileNotFoundException e) {
+         System.out.println("Error File Not Found...");
+         return null;
+      }
+   }
+
+}
--- ASGMT7/Test_Adj.java
+++ ASGMT7/Test_Adj.java
@@ -1,32 +1,36 @@
-import java.util.*;
-import java.io.*;
+import java.io.FileNotFoundException;
 
+public class Test_Adj {
 
-//simple Driver program to test Adj_List_Graph class
+   public static void main(String[] args) throws FileNotFoundException {
 
-public class Test_Adj{
-    
-  public static void main(String[] args)
-  {
-    
-    int V=5; 
-    
-    
-    Adj_List_Graph a = new Adj_List_Graph(V);
-    
-    
-    // Creating a graph with 5 vertices
-     // Adding edges one by one
-     a.addEdge(0, 1);
-     a.addEdge(0, 4);
-     a.addEdge(1, 2);
-     a.addEdge(1, 3);
-     a.addEdge(1, 4);
-     a.addEdge(2, 3);
-     a.addEdge(3, 4);
-    
-    
-    a.printGraph();
-  }
+      Adj_List_Graph k = Adj_List_Graph.file_Intake();
+      System.out.print("\nOrigonal Graph");
+      k.printGraph();
+
+      System.out.print("\n\nGraph^2");
+      Adj_List_Graph doubled = k.graphSquared();
+      doubled.printGraph();
+
+   }
+
 }
+/** 'Test_Adj'
+Input File Name : input-7-2.txt
 
+Origonal Graph
+Adjacency list of vertex 0  :  head -> 1
+Adjacency list of vertex 1  :  head -> 2 -> 3
+Adjacency list of vertex 2  :  head
+Adjacency list of vertex 3  :  head -> 4
+Adjacency list of vertex 4  :  head
+
+Graph^2
+Adjacency list of vertex 0  :  head -> 1 -> 2 -> 3
+Adjacency list of vertex 1  :  head -> 2 -> 3 -> 4
+Adjacency list of vertex 2  :  head
+Adjacency list of vertex 3  :  head -> 4
+Adjacency list of vertex 4  :  head
+
+PS C:\Users\Rwils\Desktop\Assign7>
+ */
