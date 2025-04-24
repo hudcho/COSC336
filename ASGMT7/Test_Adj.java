d2adc2af0f0d08b5a09717a0adc38f9cfcbc50e0
Jesse Washburn <142361664+jessewashburn@users.noreply.github.com>
2025-04-24 12:13:17 -0400

    Update Test_Adj.java

--- ASGMT7/Test_Adj.java
+++ ASGMT7/Test_Adj.java
@@ -11,38 +11,5 @@ public class Test_Adj {
       System.out.print("\n\nGraph^2");
       Adj_List_Graph doubled = k.graphSquared();
       doubled.printGraph();
-
    }
-
 }
-/** 'Test_Adj'
-Input File Name : input-7-2.txt
-
-Origonal Graph
-Adjacency list of vertex 0  :  head -> 1
-Adjacency list of vertex 1  :  head -> 2 -> 3
-Adjacency list of vertex 2  :  head
-Adjacency list of vertex 3  :  head -> 4
-Adjacency list of vertex 4  :  head
-
-Graph^2
-Adjacency list of vertex 0  :  head -> 1 -> 2 -> 3
-Adjacency list of vertex 1  :  head -> 2 -> 3 -> 4
-Adjacency list of vertex 2  :  head
-Adjacency list of vertex 3  :  head -> 4
-Adjacency list of vertex 4  :  head
-
-*/
-/** 'Test_Adj' 
-Input File Name : input-7-1.txt
-
-Origonal Graph
-Adjacency list of vertex 0  :  head -> 1
-Adjacency list of vertex 1  :  head -> 2
-Adjacency list of vertex 2  :  head
-
-Graph^2
-Adjacency list of vertex 0  :  head -> 1 -> 2
-Adjacency list of vertex 1  :  head -> 2
-Adjacency list of vertex 2  :  head
- */
